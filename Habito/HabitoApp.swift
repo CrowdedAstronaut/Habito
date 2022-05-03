67c3e281fb50532c4d90a4d68e38c02ed953f569
CrowdedAstronaut <ross@johnrossphillips.com>
2022-05-03 07:46:08 -0500

    Initial Commit

---
+++ Habito/HabitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoApp.swift
+//  Habito
+//
+//  Created by Ross Phillips on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
